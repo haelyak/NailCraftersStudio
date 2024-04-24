81cae0ffcd1a8fe4f922c54eb9423bc9e4385e50
haelyak <ktsai@hmc.edu>
2024-04-23 17:38:59 -0700

    tried to put canvas on nail image

--- DrawingApp/client/script.js
+++ DrawingApp/client/script.js
@@ -3,7 +3,7 @@
 
     var canvas = this.__canvas = new fabric.Canvas('canvas', {
         isDrawingMode: true,
-        backgroundImage: "https://i.pinimg.com/736x/97/f5/98/97f598a33ebd734731cd92c1ba778d2c.jpg",
+        // backgroundImage: "https://i.pinimg.com/736x/97/f5/98/97f598a33ebd734731cd92c1ba778d2c.jpg",
     });
 
 
@@ -187,4 +187,70 @@
             color: drawingShadowColorEl.value,
         });
     }
-})();
+    /**
+     * Method that adds an image to the T-Shirt canvas from a web URL.
+     * 
+     * @param {String} imageUrl      The server URL of the image that you want to load on the T-Shirt.
+     *
+     * @return {void} Return value description.
+     */
+    function updateNailImage(imageURL) {
+        fabric.Image.fromURL(imageURL, function(img) {
+            img.scaleToHeight(300);
+            img.scaleToWidth(300);
+            canvas.centerObject(img);
+            canvas.add(img);
+            canvas.renderAll();
+        });
+    }
+
+    // Update the TShirt color according to the selected color by the user
+    document.getElementById("tshirt-color").addEventListener("change", function() {
+        document.getElementById("tshirt-div").style.backgroundColor = this.value;
+    }, false);
+
+    // Update the TShirt color according to the selected color by the user
+    document.getElementById("tshirt-design").addEventListener("change", function() {
+
+        // Call the updateTshirtImage method providing as first argument the URL
+        // of the image provided by the select
+        updateTshirtImage(this.value);
+    }, false);
+
+    // When the user clicks on upload a custom picture
+    document.getElementById('tshirt-custompicture').addEventListener("change", function(e) {
+        var reader = new FileReader();
+
+        reader.onload = function(event) {
+            var imgObj = new Image();
+            imgObj.src = event.target.result;
+
+            // When the picture loads, create the image in Fabric.js
+            imgObj.onload = function() {
+                var img = new fabric.Image(imgObj);
+
+                img.scaleToHeight(300);
+                img.scaleToWidth(300);
+                canvas.centerObject(img);
+                canvas.add(img);
+                canvas.renderAll();
+            };
+        };
+
+        // If the user selected a picture, load it
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    }, false);
+
+    // When the user selects a picture that has been added and press the DEL key
+    // The object will be removed !
+    document.addEventListener("keydown", function(e) {
+        var keyCode = e.keyCode;
+
+        if (keyCode == 46) {
+            console.log("Removing selected element on Fabric.js on DELETE key !");
+            canvas.remove(canvas.getActiveObject());
+        }
+    }, false);
+});
