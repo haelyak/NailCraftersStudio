716e9d7e4bb3abd2da32c909df89b76ae1fffa3d
haelyak <ktsai@hmc.edu>
2024-04-24 21:22:12 -0700

    made canvas nail shaped

--- DrawingApp/client/script.js
+++ DrawingApp/client/script.js
@@ -2,11 +2,23 @@
     var $ = function(id) { return document.getElementById(id) };
 
     var canvas = this.__canvas = new fabric.Canvas('canvas', {
+
         isDrawingMode: true
-            // backgroundImage: "https://i.pinimg.com/736x/97/f5/98/97f598a33ebd734731cd92c1ba778d2c.jpg",
     });
-
-
+    canvas.controlsAboveOverlay = true;
+    // var clip = new fabric.Path('M 162.824 27 L 337.176 27 C 349.359 27 359.25 36.891 359.25 49.074 L 359.25 450.926 C 359.25 463.109 349.359 473 337.176 473 L 162.824 473 C 150.641 473 140.75 463.109 140.75 450.926 L 140.75 49.074 C 140.75 36.891 150.641 27 162.824 27 Z  M 190.041 66 C 200.046 66 208.168 74.206 208.168 84.312 L 208.168 141.688 C 208.168 151.794 200.046 160 190.041 160 C 180.037 160 171.915 151.794 171.915 141.688 L 171.915 84.312 C 171.915 74.206 180.037 66 190.041 66 Z');
+    var clip = new fabric.Path("m 120.13284,206.74386 c 0.9626,20.16453 -1.83902,39.81626 -15.55368,56.10132 -23.397495,27.78257 -56.863886,37.57688 -97.247659,20.95554 -26.731376,-11.00229 -41.633546,-30.90701 -42.49274,-58.20745 -1.357078,-43.12027 -4.55766,-86.41402 -1.862778,-129.360155 2.954332,-47.080777 21.448192,-89.8916649 60.303789,-122.729712 15.350316,-12.973018 26.901462,-10.752064 41.490897,3.470063 27.876488,27.174673 44.772581,59.793877 53.254831,95.605191 4.57523,19.316248 3.14219,39.848033 3.90351,61.743273 -0.94295,25.41519 -1.36955,48.91858 -1.79617,72.42193 z", {
+        left: 176,
+        top: 40
+    });
+    clip.set({ fill: 'blue', fillRule: 'evenodd' })
+    canvas.add(clip);
+    // canvas.centerObject(clip);
+    fabric.Image.fromURL('https://cdn.pixabay.com/photo/2015/12/09/01/02/colorful-abstract-background-1084082_640.jpg', function(img) {
+        canvas.add(img);
+        canvas.clipPath = clip;
+    })
+    canvas.renderAll();
     fabric.Object.prototype.transparentCorners = false;
 
     var drawingModeEl = $('drawing-mode'),
@@ -33,8 +45,23 @@
         }
     };
 
+    function maskNail() {
+        fabric.loadSVGFromURL('output.svg', function(objects, options) {
+            console.log("red nail");
+            var shape = fabric.util.groupSVGElements(objects, options);
+            canvas.add(shape);
+            canvas.clipTo = function(ctx) {
+                shape.render(ctx);
+            };
+            canvas.setDimensions({ width: shape.width, height: shape.height });
+            canvas.renderAll();
+        });
+    };
+
+
     if (fabric.PatternBrush) {
         var vLinePatternBrush = new fabric.PatternBrush(canvas);
+        // maskNail();
         vLinePatternBrush.getPatternSrc = function() {
 
             var patternCanvas = fabric.document.createElement('canvas');
@@ -53,6 +80,7 @@
         };
 
         var hLinePatternBrush = new fabric.PatternBrush(canvas);
+        // maskNail();
         hLinePatternBrush.getPatternSrc = function() {
 
             var patternCanvas = fabric.document.createElement('canvas');
@@ -66,11 +94,12 @@
             ctx.lineTo(5, 10);
             ctx.closePath();
             ctx.stroke();
-
+            // maskNail();
             return patternCanvas;
         };
 
         var squarePatternBrush = new fabric.PatternBrush(canvas);
+        // maskNail();
         squarePatternBrush.getPatternSrc = function() {
 
             var squareWidth = 10,
@@ -82,11 +111,11 @@
 
             ctx.fillStyle = this.color;
             ctx.fillRect(0, 0, squareWidth, squareWidth);
-
             return patternCanvas;
         };
 
         var diamondPatternBrush = new fabric.PatternBrush(canvas);
+        // maskNail();
         diamondPatternBrush.getPatternSrc = function() {
 
             var squareWidth = 10,
@@ -111,9 +140,10 @@
         };
 
         var img = new Image();
-        img.src = '../assets/honey_im_subtle.png';
+        img.src = 'hand.jpg';
 
         var texturePatternBrush = new fabric.PatternBrush(canvas);
+        // maskNail();
         texturePatternBrush.source = img;
     }
 
@@ -152,6 +182,7 @@
 
     drawingColorEl.onchange = function() {
         var brush = canvas.freeDrawingBrush;
+        // maskNail();
         brush.color = this.value;
         if (brush.getPatternSrc) {
             brush.source = brush.getPatternSrc.call(brush);
@@ -176,7 +207,8 @@
 
     if (canvas.freeDrawingBrush) {
         canvas.freeDrawingBrush.color = drawingColorEl.value;
-        canvas.freeDrawingBrush.source = canvas.freeDrawingBrush.getPatternSrc.call(this);
+        // maskNail();
+        // canvas.freeDrawingBrush.source = canvas.freeDrawingBrush.getPatternSrc.call(this);
         canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
         canvas.freeDrawingBrush.shadow = new fabric.Shadow({
             blur: parseInt(drawingShadowWidth.value, 10) || 0,
@@ -186,148 +218,35 @@
             color: drawingShadowColorEl.value,
         });
     }
-
-    function updateNailImage(imageURL) {
-        fabric.Image.fromURL(imageURL, function(img) {
-            img.scaleToHeight(300);
-            img.scaleToWidth(300);
-            canvas.centerObject(img);
-            canvas.add(img);
-            canvas.renderAll();
-        });
-    }
-
-    // Update the TShirt color according to the selected color by the user
-    document.getElementById("tshirt-color").addEventListener("change", function() {
-        document.getElementById("tshirt-div").style.backgroundColor = this.value;
-    }, false);
-
-    // Update the TShirt color according to the selected color by the user
-    document.getElementById("tshirt-design").addEventListener("change", function() {
-
-        // Call the updateTshirtImage method providing as first argument the URL
-        // of the image provided by the select
-        updateTshirtImage(this.value);
-    }, false);
-
-    // When the user clicks on upload a custom picture
-    document.getElementById('tshirt-custompicture').addEventListener("change", function(e) {
+    document.getElementById('imgLoader').onchange = function handleImage(e) {
         var reader = new FileReader();
-
         reader.onload = function(event) {
             var imgObj = new Image();
             imgObj.src = event.target.result;
-
-            // When the picture loads, create the image in Fabric.js
             imgObj.onload = function() {
-                var img = new fabric.Image(imgObj);
-
-                img.scaleToHeight(300);
-                img.scaleToWidth(300);
-                canvas.centerObject(img);
-                canvas.add(img);
+                var image = new fabric.Image(imgObj);
+                image.set({
+                    angle: 0,
+                    padding: 10,
+                    cornersize: 10,
+                    height: 110,
+                    width: 110,
+                });
+                canvas.centerObject(image);
+                canvas.add(image);
                 canvas.renderAll();
-            };
-        };
-
-        // If the user selected a picture, load it
-        if (e.target.files[0]) {
-            reader.readAsDataURL(e.target.files[0]);
+            }
         }
-    }, false);
-
-    // When the user selects a picture that has been added and press the DEL key
-    // The object will be removed !
-    document.addEventListener("keydown", function(e) {
-        var keyCode = e.keyCode;
-
-        if (keyCode == 46) {
-            console.log("Removing selected element on Fabric.js on DELETE key !");
-            canvas.remove(canvas.getActiveObject());
-        }
-    }, false);
-})();
-
-(function() {
-    var canvas = this.__canvas = new fabric.Canvas('canvas', {
-        hoverCursor: 'pointer',
-        selection: false,
-        targetFindTolerance: 2
-    });
-
-    canvas.on({
-        'object:moving': function(e) {
-            e.target.opacity = 0.5;
-        },
-        'object:modified': function(e) {
-            e.target.opacity = 1;
-        }
-    });
-
-    for (var i = 0, len = 15; i < len; i++) {
-        fabric.Image.fromURL('http://fabricjs.com/per-pixel-drag-drop/assets/ladybug.png', function(img) {
-            img.set({
-                left: fabric.util.getRandomInt(0, 600),
-                top: fabric.util.getRandomInt(0, 500),
-                angle: fabric.util.getRandomInt(0, 90)
-            });
-
-            img.perPixelTargetFind = true;
-            img.hasControls = img.hasBorders = false;
-
-            img.scale(fabric.util.getRandomInt(50, 100) / 100);
-
-            canvas.add(img);
-        });
-    }
-})();
-
-// Update the TShirt color according to the selected color by the user
-document.getElementById("tshirt-color").addEventListener("change", function() {
-    document.getElementById("tshirt-div").style.backgroundColor = this.value;
-}, false);
-
-// Update the TShirt color according to the selected color by the user
-document.getElementById("tshirt-design").addEventListener("change", function() {
-
-    // Call the updateTshirtImage method providing as first argument the URL
-    // of the image provided by the select
-    updateTshirtImage(this.value);
-}, false);
-
-// When the user clicks on upload a custom picture
-document.getElementById('tshirt-custompicture').addEventListener("change", function(e) {
-    var reader = new FileReader();
-
-    reader.onload = function(event) {
-        var imgObj = new Image();
-        imgObj.src = event.target.result;
-
-        // When the picture loads, create the image in Fabric.js
-        imgObj.onload = function() {
-            var img = new fabric.Image(imgObj);
-
-            img.scaleToHeight(300);
-            img.scaleToWidth(300);
-            canvas.centerObject(img);
-            canvas.add(img);
-            canvas.renderAll();
-        };
-    };
-
-    // If the user selected a picture, load it
-    if (e.target.files[0]) {
         reader.readAsDataURL(e.target.files[0]);
     }
-}, false);
+    fabric.loadSVGFromURL('output.svg', function(objects, options) {
+        console.log("red nail");
+        var shape = fabric.util.groupSVGElements(objects, options);
+        canvas.add(shape);
+        canvas.clipTo = function(ctx) {
+            shape.render(ctx);
+        };
+        canvas.renderAll();
+    });
 
-// When the user selects a picture that has been added and press the DEL key
-// The object will be removed !
-document.addEventListener("keydown", function(e) {
-    var keyCode = e.keyCode;
-
-    if (keyCode == 46) {
-        console.log("Removing selected element on Fabric.js on DELETE key !");
-        canvas.remove(canvas.getActiveObject());
-    }
-}, false);
+})();
