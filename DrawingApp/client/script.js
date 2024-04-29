aafa2583d00044c9ee75510cea43a5ebbf948390
haelyak <ktsai@hmc.edu>
2024-04-29 13:33:02 -0700

    can only see delete button in select mode

--- DrawingApp/client/script.js
+++ DrawingApp/client/script.js
@@ -5,8 +5,8 @@
 
         isDrawingMode: true
     });
+
     canvas.controlsAboveOverlay = true;
-    // var clip = new fabric.Path('M 162.824 27 L 337.176 27 C 349.359 27 359.25 36.891 359.25 49.074 L 359.25 450.926 C 359.25 463.109 349.359 473 337.176 473 L 162.824 473 C 150.641 473 140.75 463.109 140.75 450.926 L 140.75 49.074 C 140.75 36.891 150.641 27 162.824 27 Z  M 190.041 66 C 200.046 66 208.168 74.206 208.168 84.312 L 208.168 141.688 C 208.168 151.794 200.046 160 190.041 160 C 180.037 160 171.915 151.794 171.915 141.688 L 171.915 84.312 C 171.915 74.206 180.037 66 190.041 66 Z');
 
     var clip = new fabric.Path("m 123.09916,212.65982 c 0.98065,20.65785 -1.8735,40.79035 -15.84531,57.47384 -23.836198,28.46225 -57.930084,38.49618 -99.0710526,21.4682 -27.2325894,-11.27146 -42.4141744,-31.66314 -43.2894784,-59.63147 -1.382524,-44.17521 -4.643116,-88.52813 -1.897705,-132.524941 3.009725,-48.232602 21.850345,-92.0908513 61.434484,-125.732276 15.638134,-13.290401 27.405864,-11.015111 42.268851,3.554957 28.399172,27.8394977 45.612071,61.256726 54.253361,97.944157 4.66102,19.788818 3.2011,40.822913 3.9767,63.253803 -0.96063,26.03698 -1.39523,50.11538 -1.82985,74.19373 z", {
         left: 174,
@@ -52,6 +52,8 @@
             drawingModeEl.innerHTML = 'Enter drawing mode';
             drawingOptionsEl.style.display = 'none';
         }
+        updateDeleteButtonVisibility(); // Add this line
+
     };
 
 
@@ -59,7 +61,6 @@
 
     if (fabric.PatternBrush) {
         var vLinePatternBrush = new fabric.PatternBrush(canvas);
-        // maskNail();
         vLinePatternBrush.getPatternSrc = function() {
 
             var patternCanvas = fabric.document.createElement('canvas');
@@ -78,7 +79,6 @@
         };
 
         var hLinePatternBrush = new fabric.PatternBrush(canvas);
-        // maskNail();
         hLinePatternBrush.getPatternSrc = function() {
 
             var patternCanvas = fabric.document.createElement('canvas');
@@ -92,12 +92,10 @@
             ctx.lineTo(5, 10);
             ctx.closePath();
             ctx.stroke();
-            // maskNail();
             return patternCanvas;
         };
 
         var squarePatternBrush = new fabric.PatternBrush(canvas);
-        // maskNail();
         squarePatternBrush.getPatternSrc = function() {
 
             var squareWidth = 10,
@@ -113,7 +111,6 @@
         };
 
         var diamondPatternBrush = new fabric.PatternBrush(canvas);
-        // maskNail();
         diamondPatternBrush.getPatternSrc = function() {
 
             var squareWidth = 10,
@@ -141,7 +138,6 @@
         img.src = 'hand.jpg';
 
         var texturePatternBrush = new fabric.PatternBrush(canvas);
-        // maskNail();
         texturePatternBrush.source = img;
     }
 
@@ -168,54 +164,27 @@
                 brush.source = brush.getPatternSrc.call(brush);
             }
             brush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-            // brush.shadow = new fabric.Shadow({
-            //     blur: parseInt(drawingShadowWidth.value, 10) || 0,
-            //     offsetX: 0,
-            //     offsetY: 0,
-            //     affectStroke: true,
-            //     color: drawingShadowColorEl.value,
-            // });
         }
     };
 
     drawingColorEl.onchange = function() {
         var brush = canvas.freeDrawingBrush;
-        // maskNail();
         brush.color = this.value;
         if (brush.getPatternSrc) {
             brush.source = brush.getPatternSrc.call(brush);
         }
     };
-    // drawingShadowColorEl.onchange = function() {
-    //     canvas.freeDrawingBrush.shadow.color = this.value;
-    // };
+
     drawingLineWidthEl.onchange = function() {
         canvas.freeDrawingBrush.width = parseInt(this.value, 10) || 1;
         this.previousSibling.innerHTML = this.value;
     };
-    // drawingShadowWidth.onchange = function() {
-    //     canvas.freeDrawingBrush.shadow.blur = parseInt(this.value, 10) || 0;
-    //     this.previousSibling.innerHTML = this.value;
-    // };
-    // drawingShadowOffset.onchange = function() {
-    //     canvas.freeDrawingBrush.shadow.offsetX = parseInt(this.value, 10) || 0;
-    //     canvas.freeDrawingBrush.shadow.offsetY = parseInt(this.value, 10) || 0;
-    //     this.previousSibling.innerHTML = this.value;
-    // };
 
     if (canvas.freeDrawingBrush) {
         canvas.freeDrawingBrush.color = drawingColorEl.value;
-        // maskNail();
-        // canvas.freeDrawingBrush.source = canvas.freeDrawingBrush.getPatternSrc.call(this);
         canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-        // canvas.freeDrawingBrush.shadow = new fabric.Shadow({
-        //     blur: parseInt(drawingShadowWidth.value, 10) || 0,
-        //     offsetX: 0,
-        //     offsetY: 0,
-        //     affectStroke: true,
-        //     color: drawingShadowColorEl.value,
-        // });
     }
+
     document.getElementById('imgLoader').onchange = function handleImage(e) {
         var reader = new FileReader();
         reader.onload = function(event) {
@@ -237,7 +206,7 @@
         }
         reader.readAsDataURL(e.target.files[0]);
     }
-    let lockHistory = false; //Undo/Redo時の描画イベントに反応させないためのフラグ
+    let lockHistory = false;
     const undo_history = [];
     const redo_history = [];
 
@@ -262,7 +231,7 @@
     function undo() {
         if (undo_history.length > 0) {
             lockHistory = true;
-            if (undo_history.length > 1) redo_history.push(undo_history.pop()); //最初の白紙はredoに入れない
+            if (undo_history.length > 1) redo_history.push(undo_history.pop());
             const content = undo_history[undo_history.length - 1];
             canvas.loadFromJSON(content, function() {
                 canvas.renderAll();
@@ -283,26 +252,49 @@
         }
     }
 
+    // deleting objects
+    var deleteEl = function() {
+        var activeObject = canvas.getActiveObject();
+        if (activeObject) {
+            canvas.remove(activeObject);
+            canvas.renderAll();
+        }
+    };
+
+    // assigns the deleteEl function to the button
+    var deleteObjectEl = $('delete-object');
+    if (deleteObjectEl) {
+        deleteObjectEl.onclick = deleteEl;
+    }
+
+    function updateDeleteButtonVisibility() {
+        if (canvas.isDrawingMode) {
+            deleteObjectEl.style.display = 'none';
+        } else {
+            deleteObjectEl.style.display = '';
+        }
+    }
+
+    updateDeleteButtonVisibility(); // Add this line
+
+
+
     document.getElementById("undo").addEventListener("click", undo);
     document.getElementById("redo").addEventListener("click", redo);
 
-    //編集モードボタンの処理
+
     document.getElementById("editMode").addEventListener("mouseup", function(e) {
         if (canvas.isDrawingMode) {
             canvas.isDrawingMode = false;
-            // clearSelectedButton();
-            this.classList.add("selected"); //選択されたボタンはボーダーを太くする
+            this.classList.add("selected");
         } else {
             canvas.isDrawingMode = true;
-            // clearSelectedButton();
-            // lastSelectdColorBtn.classList.add("selected");
-            this.classList.remove("selected"); //選択されたボタンはボーダーを太くする
+            this.classList.remove("selected");
             canvas.discardActiveObject();
             canvas.requestRenderAll();
         }
     });
 
-    //deleteボタンの処理
     const deleteBtn = document.getElementById("delete");
 
     canvas.on("selection:created", function() {
@@ -324,11 +316,10 @@
         });
         canvas.discardActiveObject();
         canvas.requestRenderAll();
-        undo_history.push(JSON.stringify(canvas)); //UNDO処理
+        undo_history.push(JSON.stringify(canvas));
         lockHistory = false;
     }
 
-    //Deteleキーの処理
     document.addEventListener("keyup", function(e) {
         console.log(e.keyCode);
         if ((e.keyCode == 8) | (e.keyCode == 46)) {
